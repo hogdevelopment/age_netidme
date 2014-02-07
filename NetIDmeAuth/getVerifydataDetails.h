9ca080f4570ee060543aa5529a311806b3302441
shinoyvj2 <shinoy.vj2@gmail.com>
2014-02-07 17:35:39 +0000

    Adding 2nd time
    adding netIDme files

---
+++ NetIDmeAuth/getVerifydataDetails.h
@@ -0,0 +1,18 @@
+//
+//  getVerifydataDetails.h
+//  testNetIDme
+//
+//  Created by shinoy on 1/30/14.
+//  Copyright (c) 2014 SHINOY. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "NetIDVerifyServiceSvc.h"
+
+
+@interface getVerifydataDetails : NSObject
+
+
++(NetIDVerifyServiceSvc_VerificationData *)getData;
+
+@end
---
+++ NetIDmeAuth/tns2.h
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+#import "USAdditions.h"
+#import <libxml/tree.h>
+#import "USGlobals.h"
+@class tns2_ArrayOfstring;
